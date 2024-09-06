004ae9171f43d7775f49118ec7e3822edcf1c8d4
oboitsun <thewip3out@gmail.com>
2024-09-06 19:20:58 +0300

    removed some sections

--- components/Sections/PromoVideoSection.js
+++ components/Sections/PromoVideoSection.js
@@ -8,7 +8,7 @@ export default function PromoVideoSection() {
   const inView = useInView(ref, { amount: "all", once: true });
   const [show, setShow] = useState(false);
   return (
-    <section className="pt-10 lg:pt-20 xl:pt-28 bg-mustard">
+    <section className="py-10 lg:py-20 xl:py-28 bg-mustard">
       <div className="wrapper flex flex-col gap-5 lg:gap-10">
         <h2 className="heading  text-black text-center">ONE LOVE 2025 - VIDEO ANNOUNCEMENT</h2>
         <div ref={ref} className="w-full bg-black rounded-10 overflow-hidden">

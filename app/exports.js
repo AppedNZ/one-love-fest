8f5eb725701c02be4b487018a1644cd96b67fcf8
oboitsun <thewip3out@gmail.com>
2024-09-23 14:09:36 +0300

    hidden camping page

--- app/exports.js
+++ app/exports.js
@@ -1,10 +1,10 @@
-import { ABOUT, CAMPING, CONTACT, FAQ, NEWS, SITEMAP, TICKETS } from "./constants";
+import { ABOUT, CONTACT, FAQ, NEWS, SITEMAP, TICKETS } from "./constants";
 
 export const header_links = [
   { url: "/", label: "Home" },
   { url: ABOUT, label: "About" },
   { url: TICKETS, label: "Tickets" },
-  { url: CAMPING, label: "Camping" },
+  // { url: CAMPING, label: "Camping" },
   // { url: PAYMENT_PLAN, label: "Payment Plan" },
   { url: FAQ, label: "FAQ" },
   { url: NEWS, label: "News" },
